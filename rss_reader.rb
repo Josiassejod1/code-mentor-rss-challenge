ce4055ed49585151b37adc75dc1372b162c7f30d
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2023-04-19 21:25:05 -0700

    add new line spacing, handle errors

--- rss_reader.rb
+++ rss_reader.rb
@@ -1,32 +1,38 @@
 require 'rss'
 require 'open-uri'
 
-
-# completing this as a part of code mentors
 def process_args(argument)
-    batch = []
+  batch = []
+    
+  if valid_url?(argument) && argument.length > 1
+    batch.push(argument)
+  end
 
-    if argument =~ URI::regexp
-        puts argument
-        parse_url(argument)
-    end
+  batch.each do |url|
+    parse_url(url)
+  end
+end
+
+def valid_url?(url)
+  url =~ URI::regexp
 end
 
 def parse_url(url)
-    URI.open(url) do |rss|
-        feed = RSS::Parser.parse(rss)
-        puts "Title: #{feed.channel.title}"
-        puts "Description: #{feed.channel.description}"
-        puts "Link: #{feed.channel.link}"
+  URI.open(url) do |rss|
+    feed = RSS::Parser.parse(rss)
+    puts "\n"
+    puts "Title: #{feed.channel.title}"
+    puts "Description: #{feed.channel.description}"
+    puts "Link: #{feed.channel.link}"
 
-        rescue RSS::Error
+    rescue RSS::Error
+      puts "Unknown RSS Error"
+   end
 
-        puts "Unknown error"
-    end
+  rescue OpenURI::HTTPError 
+    puts "Couldn't parse url"
 end
 
 ARGV.each { |option| process_args(option)}
-
-@options = {}
 
 
